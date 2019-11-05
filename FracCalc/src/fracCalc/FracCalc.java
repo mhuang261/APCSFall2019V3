935cc8ab673bb51dd1b572379827bf5742c0d278
APCS3rd <APCS3rd@MHS-LT9-35>
2019-11-05 12:03:42 -0800

    //fracalc checkpoint 3 almost; need to account for whole numbers

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -34,8 +34,7 @@ public class FracCalc {
     //        
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    { 
+    public static String produceAnswer(String input) { 
     	//first level takes inputs
         String operand1 = "";
         String operand2 = "";
@@ -54,34 +53,52 @@ public class FracCalc {
         String denom1 = splitFrac1[1];   
         String whole1 = "";
         
+        
+        
+        //2ND FRACTION
+                	
         //describing 2nd fraction
+
         String[] splitFrac2 = operand2.split("/"); //  now I have 1_1, 2
         
         String num2 = splitFrac2[0]; //could be more than a num  
-        String denom2 = splitFrac2[1];
+        String denom2 = splitFrac2[1];// operand2 = 20 if 
         String whole2 = "";
+        
         
         //TESTS IF IT IS FRACTION
         String[] splitFrac2Num = splitFrac2[0].split("_"); //splits index 0 at underscore (1, 1)
         
-        if (splitFrac2Num.length == 1) {//must be fraction
-        	splitFrac2Num[0] = "0_" + splitFrac2Num[0]; // length will be one, index 0 have a whole number of 0 added to it
-        	
-        }
+        if (splitFrac2Num.length == 1) {//must be fraction or an integer
+        	String[] splitFrac2NumWholeTest = splitFrac2Num[0].split("/"); //splits num at _
+        	//CHECKS FOR FRACTION
+        	if (splitFrac2NumWholeTest.length > 1) {
+        		splitFrac2Num[0] = "0_" + splitFrac2Num[0]; // 1 (1) --> 0_1 given (1/3)
+        		String[] splitFrac2NumWhole = (splitFrac2Num[0]).split("_"); //0_1 splits at _
+        		whole2 = splitFrac2NumWhole[0]; //0_1
+        		num2 = splitFrac2NumWhole[1];
+        	}
+        	//CHECKS FOR INTEGER
+        	if (splitFrac2NumWholeTest.length == 1) { 
+            	String[] splitFrac2NumWhole = (splitFrac2Num[0]).split("_"); //split fraction at 
+            	whole2 = splitFrac2NumWhole[0]; 
+            	num2 = "0";
+            	denom2 = "1";
+     	}
+  }
         
         //TESTS IF IT IS MIXED NUM & ASSIGNS WHOLE NUMBER AND NUM (DENOM ALREADY ASSIGNED ABOVE)
         
         if (splitFrac2Num.length != 1) { //must have a whole number and therefore be a mixed number
-        	whole2 = splitFrac2Num[0]; //whole number will equal the whole number
+        	whole2 = splitFrac2Num[0]; //whole number will equal the whole number 
         	num2 = splitFrac2Num[1];
         }
-       
-      
+             
         //NEED TO CONVERT BACK TO INT
-        int num2Int = 
-        int denom2Int = 
-        int whole2Int = parseInt(whole2);
-        return ;
+        int num2Int = Integer.parseInt(num2);
+        int denom2Int = Integer.parseInt(denom2);
+        int whole2Int = Integer.parseInt(whole2);
+        return "whole:" + whole2Int + " " + "numerator:" + num2Int + " " + "denominator:" + denom2Int;
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need

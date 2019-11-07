1d50630fa26138be97964d1515e7e372a6bc12a4
APCS3rd <APCS3rd@MHS-LT9-35>
2019-11-07 10:59:22 -0800

    working on checkpoint3

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -44,12 +44,54 @@ public class FracCalc {
         
         //splits first level of inputs
         
-        String[] splitFrac1 = operand1.split("/"); //declaring and splitting operand 1/2 into (1, 2)
-        //describing 1st fraction
-        String num1 = "";
-        String denom1 = "";   
+        //1ST FRACTION
+    	
+        //describing 1st fraction        
+        String num1 = ""; //could be more than a single digit, may be 5_1
+        String denom1 = "";// can either be 1 if whole number or something like 5 if fraction (1/5)
         String whole1 = "";
         
+        
+        //FRACTION, INTEGER AND MIXED NUMBER TESTS
+        String[] splitFrac1 = operand1.split("/"); //  if operand 2 is 21/2 then (21, 2)
+        String[] splitFrac1Num = splitFrac1[0].split("_"); //splits index 0 at underscore like (5, 1) for 5_1 or (21) for 21,2
+        
+        //TESTS IF IT IS FRACTION OR INTEGER
+        if (splitFrac1Num.length == 1) {//must be fraction or an integer (21/2 or 20)
+       
+        	//CHECKS FOR FRACTION LIKE 21/2 OR 1/2
+        	if (splitFrac1.length > 1) {
+        		splitFrac1Num[0] = "0_" + splitFrac1Num[0]; // given (1/2) --> (1) ==> (0_1)
+        		String[] splitFrac1NumSingle= (splitFrac1Num[0]).split("_"); //0_1 splits into (0, 1)
+        		whole1 = splitFrac1NumSingle[0]; 
+        		num1 = splitFrac1NumSingle[1];
+        		denom1 = splitFrac1[1];
+        	}
+        	
+        	//CHECKS FOR WHOLE NUMBER LIKE 20 (INTEGER)
+
+        	if (splitFrac1.length == 1) {
+        		whole1 = splitFrac1[0];
+        		num1 = "0";
+        		denom1 = "1";
+        	}
+        }
+     
+
+        	//CHECKS FOR MIXED NUMBER LIKE 5_1/2
+        	if (splitFrac1Num.length > 1 ) { // all mixed numbers will pass through here
+        		
+            	whole1 = splitFrac1Num[0]; 
+            	num1 = splitFrac1Num[1];
+            	denom1 = splitFrac1[1];
+     	}
+        
+        	//PARSE FIRST FRACTION
+        	
+        	//NEED TO CONVERT BACK TO INT
+            int num1Int = Integer.parseInt(num1);
+            int denom1Int = Integer.parseInt(denom1);
+            int whole1Int = Integer.parseInt(whole1);
         
         
         //2ND FRACTION
@@ -77,6 +119,7 @@ public class FracCalc {
         		whole2 = splitFrac2NumSingle[0]; 
         		num2 = splitFrac2NumSingle[1];
         		denom2 = splitFrac2[1];
+        		
         	}
         	
         	//CHECKS FOR WHOLE NUMBER LIKE 20 (INTEGER)
@@ -98,13 +141,52 @@ public class FracCalc {
      	}
              
         //NEED TO CONVERT BACK TO INT
+        int whole2Int = Integer.parseInt(whole2);
         int num2Int = Integer.parseInt(num2);
         int denom2Int = Integer.parseInt(denom2);
-        int whole2Int = Integer.parseInt(whole2);
+        
+        //MIXED NUMBER CONVERT TO IMPROPER IF 
+        if (whole2Int != 0) { // if the whole number is not zero, then will call change mixed number into improper fraction
+			num2Int = MixImp(whole2Int, num2Int, denom2Int);
+		}else if (whole1Int != 0) {
+			whole1Int = MixImp(whole1Int, num1Int, denom2Int);
+		}
 
         return "whole:" + whole2Int + " " + "numerator:" + num2Int + " " + "denominator:" + denom2Int;
     }
+    
 
-    // TODO: Fill in the space below with any helper methods that you think you will need
+    // TODO: Fill in the space below with any helper methods that you think you will need 
+    //EXTRA METHODS GO HERE
+    
+    	//MIXED TO IMPROPER (5_1/2 --> 11/2)
+    	public static int MixImp (int whole, int num, int denom) {
+    		int improperNum = whole*denom + num;
+    		return improperNum;
+    	}
+    	
+    	//ADDITION
+        public static String add (int whole, int num, int denom) {
+        	
+        	return "";
+        }
+        
+        //SUBTRACTION
+        public static String sub (int whole, int num, int denom) {
+        	
+        	return "";
+        }
+        //MULTIPLICATION
+        public static String mult (int whole, int num, int denom) {
+        	
+        	return "";
+        }
+        //DIVISION 
+        public static String divide (int whole, int num, int denom) {
+        	
+        	return "";
+        }
+        
+    
     
 }

a041024d3654d0052bb8a29cfb43c6398f6434ef
APCS3rd <APCS3rd@MHS-LT9-35>
2020-02-05 10:57:54 -0800

    //writing beginning code

---
+++ TheHobbitRevised/src/ThereAndBackAgain.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class ThereAndBackAgain 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		Hobbit frodo = new Hobbit("Frodo");
+		Hobbit sam = new Hobbit("Sam");
+		Dwarf gimli = new Dwarf("Gimli");
+		
+		// Create a traveling party called party1 by creating an array of Travelers 
+		// and filling it with frodo, sam, and gimli
+		// Then, use a loop to make all travelers go a distance of 50 miles  
+		// Then, for each Traveler in the travelingParty, print their name and how far they've
+		//    traveled in miles.  (In the next piece, you'll do this in methods, but 
+		//    for a first pass, just do it in main and print to the console.)
+		// Expected output:  Frodo has traveled 50 miles.
+		//                   Sam has traveled 50 miles.
+		//                   Gimli has traveled 50 miles.
+		
+		Traveler[] party1 = {frodo, sam, gimli};
+			for (int i = 0; i < party1.length; i++) {
+				party1[i].travel(50);
+				System.out.println(party1[i].getName() + " has traveled " + party1[i].getDistanceTraveled() + " miles.");
+			}
+		
+		
+		
+		System.out.println();
+
+	}
+
+	
+}

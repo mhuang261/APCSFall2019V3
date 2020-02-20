6e8d92dec9a0b515ad539bbf6c33908242b90822
Michael Huang <mhuang261@gmail.com>
2020-02-19 23:19:54 -0800

    //fixed call in hobbit method

--- TheHobbitRevised/src/Hobbit.java
+++ TheHobbitRevised/src/Hobbit.java
@@ -5,11 +5,12 @@ public class Hobbit extends Traveler
 	//   set the value of ringobssessed to false.  
 	//Hobbits travel just like any other Traveler. Do they need a travel method?  
 	//  Try it without and find out.  =]  
-	
+
 	private boolean ringObsessed;
 	
 	public Hobbit(String name)
 	{
 		super(name);
+		ringObsessed = false;
 	}
 }
